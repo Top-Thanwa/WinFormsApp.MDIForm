3ff57c0980a4f4f646b49184f1a6ddeba0373eb8
Top-Thanwa <phanetramtop@gmail.com>
2026-01-14 11:32:03 +0700

    Add project files.

---
+++ WinFormsApp.MDIForm/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WinFormsApp.MDIForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            mnuForm = new ToolStripMenuItem();
+            mnuFromMax = new ToolStripMenuItem();
+            mnuFromMin = new ToolStripMenuItem();
+            mnuFormNormal = new ToolStripMenuItem();
+            ปดโปรแกรมToolStripMenuItem = new ToolStripSeparator();
+            mnuFormClose = new ToolStripMenuItem();
+            mnuFormCloseAll = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripSeparator();
+            mnuExit = new ToolStripMenuItem();
+            mnuFormA = new ToolStripMenuItem();
+            เปดฟอรมAToolStripMenuItem = new ToolStripMenuItem();
+            mnuFormB = new ToolStripMenuItem();
+            mnuFormC = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { mnuForm, mnuFormA, mnuFormB, mnuFormC });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(994, 28);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuForm
+            // 
+            mnuForm.DropDownItems.AddRange(new ToolStripItem[] { mnuFromMax, mnuFromMin, mnuFormNormal, ปดโปรแกรมToolStripMenuItem, mnuFormClose, mnuFormCloseAll, toolStripMenuItem1, mnuExit });
+            mnuForm.Name = "mnuForm";
+            mnuForm.Size = new Size(55, 24);
+            mnuForm.Text = "ฟอร์ม";
+            // 
+            // mnuFromMax
+            // 
+            mnuFromMax.Name = "mnuFromMax";
+            mnuFromMax.Size = new Size(224, 26);
+            mnuFromMax.Text = "ขยาย";
+            mnuFromMax.Click += mnuFromMax_Click;
+            // 
+            // mnuFromMin
+            // 
+            mnuFromMin.Name = "mnuFromMin";
+            mnuFromMin.Size = new Size(224, 26);
+            mnuFromMin.Text = "ย่อ";
+            mnuFromMin.Click += mnuFromMin_Click;
+            // 
+            // mnuFormNormal
+            // 
+            mnuFormNormal.Name = "mnuFormNormal";
+            mnuFormNormal.Size = new Size(224, 26);
+            mnuFormNormal.Text = "ปรับปกติ";
+            mnuFormNormal.Click += mnuFormNormal_Click;
+            // 
+            // ปดโปรแกรมToolStripMenuItem
+            // 
+            ปดโปรแกรมToolStripMenuItem.Name = "ปดโปรแกรมToolStripMenuItem";
+            ปดโปรแกรมToolStripMenuItem.Size = new Size(221, 6);
+            // 
+            // mnuFormClose
+            // 
+            mnuFormClose.Name = "mnuFormClose";
+            mnuFormClose.Size = new Size(224, 26);
+            mnuFormClose.Text = "ปิดฟอร์ม";
+            mnuFormClose.Click += mnuFormClose_Click;
+            // 
+            // mnuFormCloseAll
+            // 
+            mnuFormCloseAll.Name = "mnuFormCloseAll";
+            mnuFormCloseAll.Size = new Size(224, 26);
+            mnuFormCloseAll.Text = "ปิดฟอร์มทั้งหมด";
+            mnuFormCloseAll.Click += mnuFormCloseAll_Click;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(221, 6);
+            // 
+            // mnuExit
+            // 
+            mnuExit.Name = "mnuExit";
+            mnuExit.Size = new Size(224, 26);
+            mnuExit.Text = "ปิดโปรแกรม";
+            mnuExit.Click += mnuExit_Click;
+            // 
+            // mnuFormA
+            // 
+            mnuFormA.DropDownItems.AddRange(new ToolStripItem[] { เปดฟอรมAToolStripMenuItem });
+            mnuFormA.Name = "mnuFormA";
+            mnuFormA.Size = new Size(91, 24);
+            mnuFormA.Text = "เปิดฟอร์ม A";
+            mnuFormA.Click += mnuFormA_Click;
+            // 
+            // เปดฟอรมAToolStripMenuItem
+            // 
+            เปดฟอรมAToolStripMenuItem.Name = "เปดฟอรมAToolStripMenuItem";
+            เปดฟอรมAToolStripMenuItem.Size = new Size(160, 26);
+            เปดฟอรมAToolStripMenuItem.Text = "เปิดฟอร์ม A";
+            // 
+            // mnuFormB
+            // 
+            mnuFormB.Name = "mnuFormB";
+            mnuFormB.Size = new Size(84, 24);
+            mnuFormB.Text = "เปิดฟอ์ม B";
+            mnuFormB.Click += mnuFormB_Click;
+            // 
+            // mnuFormC
+            // 
+            mnuFormC.Name = "mnuFormC";
+            mnuFormC.Size = new Size(90, 24);
+            mnuFormC.Text = "เปิดฟอร์ม C";
+            mnuFormC.Click += mnuFormC_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(994, 488);
+            Controls.Add(menuStrip1);
+            Font = new Font("Leelawadee UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "ทดสอบฟอร์ม MDI";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem mnuForm;
+        private ToolStripMenuItem mnuFromMax;
+        private ToolStripMenuItem mnuFromMin;
+        private ToolStripMenuItem mnuFormA;
+        private ToolStripMenuItem mnuFormB;
+        private ToolStripSeparator ปดโปรแกรมToolStripMenuItem;
+        private ToolStripMenuItem mnuFormC;
+        private ToolStripMenuItem mnuExit;
+        private ToolStripMenuItem เปดฟอรมAToolStripMenuItem;
+        private ToolStripMenuItem mnuFormNormal;
+        private ToolStripMenuItem mnuFormClose;
+        private ToolStripMenuItem mnuFormCloseAll;
+        private ToolStripSeparator toolStripMenuItem1;
+    }
+}
---
+++ WinFormsApp.MDIForm/Form1.cs
@@ -0,0 +1,86 @@
+namespace WinFormsApp.MDIForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuFormA_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormA());
+            //FormA f1 = new FormA();
+            //f1.MdiParent = this;
+            //f1.Show();
+        }
+
+        private void mnuFormB_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormB());
+            //FormB f2 = new FormB();
+            //f2.MdiParent = this;
+            //f2.Show();
+        }
+
+        private void mnuFormC_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormC());
+            //FormC f3 = new FormC();
+            //f3.MdiParent = this;
+            //f3.Show();
+        }
+
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mnuFromMax_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void mnuFromMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void mnuFormNormal_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void mnuFormClose_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                this.ActiveMdiChild.Close();
+            }
+
+        }
+
+        private void mnuFormCloseAll_Click(object sender, EventArgs e)
+        {
+            foreach (Form frm in this.MdiChildren)
+            {
+                frm.Close();
+            }
+        }
+        private void OpenChildForm(Form formName)
+        {   
+            foreach (Form f in this.MdiChildren)
+            {
+                if (f.GetType() == formName.GetType())
+                {
+                    f.Activate();  
+                    return;
+                }
+            }
+
+            formName.MdiParent = this;
+            formName.WindowState = FormWindowState.Normal;
+            formName.Show();
+        }
+    }
+}
